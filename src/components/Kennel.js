9c3451dc91416fc73042d1e1ce5d8a228ed3ae91
Val Currie <contact@valcuri.email>
2021-06-03 13:07:59 -0500

    auth:)

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -2,12 +2,34 @@ import React from "react";
 import "./Kennel.css";
 import { NavBar } from "./nav/NavBar";
 import { ApplicationViews } from "./ApplicationViews";
+import { Route, Redirect } from "react-router";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
 export const Kennel = () => (
   <>
-    <h2>Val's Kennels</h2>
-    <small>I'll watch your dogs and I'll do it good, too!</small>
-    <NavBar />
-    <ApplicationViews />
+    <Route
+      render={() => {
+        if (localStorage.getItem("kennel_customer")) {
+          return (
+            <>
+              <h2>Val's Kennels</h2>
+              <small>I'll watch your dogs and I'll do it good, too!</small>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
   </>
 );
